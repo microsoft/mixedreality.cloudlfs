655ecabd055e5bb9044275413da27f75f162ebd6
Ali Khan <alkha@microsoft.com>
2022-08-29 11:30:56 -0700

    Initial draft.

---
+++ src/CloudLFS.Cli/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CloudLFS.Cli
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // by convention all data comes in stdin after client is invoked
+            var requestJson = Console.ReadLine();
+            
+            // todo: deserialize
+        }
+    }
+}
