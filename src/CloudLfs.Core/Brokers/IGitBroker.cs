2fc10d07d6dfd5ec7570c4f4dfc50d660fe68dec
Ali Khan <alkha@microsoft.com>
2022-09-01 12:33:28 -0700

    Added definition for git broker.

---
+++ src/CloudLfs.Core/Brokers/IGitBroker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Microsoft.MixedReality.CloudLfs.Brokers
+{
+    public interface IGitBroker
+    {
+        Task<NetworkCredential> GetCredential(string hostname, string protocol);
+
+        Task<Uri> GetLfsEndpoint();
+    }
+}
