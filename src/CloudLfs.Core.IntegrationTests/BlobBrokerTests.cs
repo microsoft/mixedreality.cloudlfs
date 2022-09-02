ca8cdd32652c2b7e9888861a7946b5f2bd960557
Ali Khan <alkha@microsoft.com>
2022-09-02 11:45:16 -0700

    Added test BlobBroker_CanDownloadFile -> FAIL

--- src/CloudLfs.Core.IntegrationTests/BlobBrokerTests.cs
+++ src/CloudLfs.Core.IntegrationTests/BlobBrokerTests.cs
@@ -20,13 +20,40 @@ namespace CloudLfs.Core.UnitTests
             var buffer = new byte[1000000];
             var rng = new Random();
             rng.NextBytes(buffer);
-            var memoryStream = new MemoryStream(buffer);
+            using var memoryStream = new MemoryStream(buffer);
 
             // act
             var successful = await broker.UploadAsync(id, new Progress<TransferStatus>(), memoryStream, CancellationToken.None);
 
             // assert
             Assert.IsTrue(successful);
+        }
+
+        [TestMethod]
+        public async Task BlobBroker_CanDownloadFile()
+        {
+            // arrange
+            var broker = new BlobBroker(new Uri("https://cloudlfscachewusint.blob.core.windows.net/"));
+            var id = Guid.NewGuid().ToString("n");
+            var buffer = new byte[1000000];
+            var rng = new Random();
+            rng.NextBytes(buffer);
+            using var uploadStream = new MemoryStream(buffer);
+            using var downloadStream = new MemoryStream();
+
+            // act
+            var uploadSucessful = await broker.UploadAsync(id, new Progress<TransferStatus>(), uploadStream, CancellationToken.None);
+            var downloadSucessful = await broker.DownloadAsync(id, new Progress<TransferStatus>(), uploadStream, CancellationToken.None);
+
+            // assert
+            Assert.IsTrue(uploadSucessful);
+            Assert.IsTrue(downloadSucessful);
+
+            uploadStream.Position = 0;
+            for (var i = 0; i < downloadStream.Length; i++)
+            {
+                Assert.AreEqual(uploadStream.ReadByte(), downloadStream.ReadByte());
+            }
         }
     }
 }
