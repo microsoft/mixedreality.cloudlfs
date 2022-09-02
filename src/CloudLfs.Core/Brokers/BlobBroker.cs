8160486be9d1850b4f7ac9e389d1a60854578415
Daniel Lopez <llopezmejia@microsoft.com>
2022-09-02 10:56:04 -0700

    Added BlobBroker class

---
+++ src/CloudLfs.Core/Brokers/BlobBroker.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.MixedReality.CloudLfs.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.MixedReality.CloudLfs.Brokers
+{
+    internal class BlobBroker : IBlobBroker
+    {
+        public Task<bool> DownloadAsync(string id, IProgress<TransferStatus> progress, Stream contentStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UploadAsync(string id, IProgress<TransferStatus> progress, Stream contentStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
