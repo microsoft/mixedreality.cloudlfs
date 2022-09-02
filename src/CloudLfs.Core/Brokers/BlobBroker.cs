7b823e38437a81c9818db387caae985882ea8a5f
Daniel Lopez <llopezmejia@microsoft.com>
2022-09-02 11:05:12 -0700

    Pass cancellationToken

--- src/CloudLfs.Core/Brokers/BlobBroker.cs
+++ src/CloudLfs.Core/Brokers/BlobBroker.cs
@@ -3,18 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.MixedReality.CloudLfs.Brokers
 {
     internal class BlobBroker : IBlobBroker
     {
-        public Task<bool> DownloadAsync(string id, IProgress<TransferStatus> progress, Stream contentStream)
+        public Task<bool> DownloadAsync(string id, IProgress<TransferStatus> progress, Stream contentStream, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> UploadAsync(string id, IProgress<TransferStatus> progress, Stream contentStream)
+        public Task<bool> UploadAsync(string id, IProgress<TransferStatus> progress, Stream contentStream, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
--- src/CloudLfs.Core/Brokers/IBlobBroker.cs
+++ src/CloudLfs.Core/Brokers/IBlobBroker.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.MixedReality.CloudLfs.Models;
 using System;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.MixedReality.CloudLfs.Brokers
@@ -13,7 +14,7 @@ namespace Microsoft.MixedReality.CloudLfs.Brokers
         /// <param name="id">The id of the content.</param>
         /// <param name="contentStream">The stream to write to.</param>
         /// <returns>True if the file was successfully downloaded, false otherwise.</returns>
-        public Task<bool> DownloadAsync(string id, IProgress<TransferStatus> progress, Stream contentStream);
+        public Task<bool> DownloadAsync(string id, IProgress<TransferStatus> progress, Stream contentStream, CancellationToken cancellationToken);
 
         /// <summary>
         /// Upload the content to the storage provider. Return FALSE if file could not be uploaded.
@@ -21,6 +22,6 @@ namespace Microsoft.MixedReality.CloudLfs.Brokers
         /// <param name="id">The id of the content.</param>
         /// <param name="contentStream">The stream to read from.</param>
         /// <returns>True if the file was successfully uploaded, false otherwise.</returns>
-        public Task<bool> UploadAsync(string id, IProgress<TransferStatus> progress, Stream contentStream);
+        public Task<bool> UploadAsync(string id, IProgress<TransferStatus> progress, Stream contentStream, CancellationToken cancellationToken);
     }
 }
