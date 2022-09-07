8ed3fdb4657d123849762a93b0fe63deb1c1a81b
Ali Khan <alkha@microsoft.com>
2022-09-07 09:58:22 -0700

    chunking draft

---
+++ src/CloudLfs.Core.IntegrationTests/BlobServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.MixedReality.CloudLfs.Brokers;
+using Microsoft.MixedReality.CloudLfs.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CloudLfs.Core.UnitTests
+{
+    [TestClass]
+    public class BlobServiceTests
+    {
+        [TestMethod]
+        public async Task BlobService_CanUploadFile()
+        {
+            // arrange
+            var broker = new BlobBroker(new Uri("https://cloudlfscachewusint.blob.core.windows.net/"));
+            var service = new BlobService(broker);
+            var id = Guid.NewGuid().ToString("n");
+            var buffer = new byte[100000000];
+            var rng = new Random();
+            rng.NextBytes(buffer);
+            using var memoryStream = new MemoryStream(buffer);
+
+            // act
+            await service.UploadAsync(id, new Progress<TransferStatus>(), memoryStream, CancellationToken.None);
+        }
+    }
+}
--- src/CloudLfs.Core/Brokers/BlobBroker.cs
+++ src/CloudLfs.Core/Brokers/BlobBroker.cs
@@ -6,12 +6,72 @@ using Microsoft.MixedReality.CloudLfs.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.MixedReality.CloudLfs.Brokers
 {
+    public interface IBlobService
+    {
+    }
+
+    public class BlobService : IBlobService
+    {
+        private readonly IBlobBroker _broker;
+
+        /// <summary>
+        /// The chunk size, in bytes.
+        /// </summary>
+        private const int DefaultChunkSize = 1000000;
+
+        private const int MaxParallelism = 4;
+
+        public BlobService(IBlobBroker broker)
+        {
+            _broker = broker;
+            ServicePointManager.DefaultConnectionLimit = Math.Max(64, ServicePointManager.DefaultConnectionLimit);
+        }
+
+        public async Task UploadAsync(string blobName, IProgress<TransferStatus> progress, Stream contentStream, CancellationToken cancellationToken)
+        {
+            // shard out the incoming stream
+            var chunkSize = DefaultChunkSize;
+            var chunkCount = contentStream.Length / chunkSize;
+            var tasks = new List<Task>();
+            var semaphore = new SemaphoreSlim(MaxParallelism, MaxParallelism);
+            for (var i = 0; i < chunkCount; i++)
+            {
+                tasks.Add(UploadChunkAsync(contentStream, chunkSize, i, semaphore, cancellationToken));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        private async Task UploadChunkAsync(Stream contentStream, int chunkSize, int chunkIndex, SemaphoreSlim semaphore, CancellationToken cancellationToken)
+        {
+            try
+            {
+                semaphore.Wait();
+                var buffer = new byte[chunkSize];
+                await contentStream.ReadAsync(buffer, chunkIndex * chunkSize, chunkSize);
+                var chunkStream = new MemoryStream(buffer);
+                var hashAlgo = SHA256.Create();
+                var hash = hashAlgo.ComputeHash(buffer);
+                var hashString = BitConverter.ToString(hash).Replace("-", string.Empty);
+                await _broker.UploadAsync(hashString, new Progress<TransferStatus>(), chunkStream, cancellationToken);
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+    }
+
     public class BlobBroker : IBlobBroker
     {
         private Uri _storageUri;
