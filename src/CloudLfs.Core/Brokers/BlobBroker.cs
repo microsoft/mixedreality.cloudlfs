9a03b5458e0d4553f71613a1bca34766f1bcaf9d
Ali Khan <alkha@microsoft.com>
2022-09-09 08:36:30 -0700

    modified blob broker

--- src/CloudLfs.Core/Brokers/BlobBroker.cs
+++ src/CloudLfs.Core/Brokers/BlobBroker.cs
@@ -2,99 +2,48 @@
 using Azure.Identity;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
 using Microsoft.MixedReality.CloudLfs.Models;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Net;
-using System.Net.Http.Headers;
-using System.Runtime.Serialization;
-using System.Security.Cryptography;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.MixedReality.CloudLfs.Brokers
 {
-    public interface IBlobService
-    {
-    }
-
-    public class BlobService : IBlobService
-    {
-        private readonly IBlobBroker _broker;
-
-        /// <summary>
-        /// The chunk size, in bytes.
-        /// </summary>
-        private const int DefaultChunkSize = 1000000;
-
-        private const int MaxParallelism = 4;
-
-        public BlobService(IBlobBroker broker)
-        {
-            _broker = broker;
-            ServicePointManager.DefaultConnectionLimit = Math.Max(64, ServicePointManager.DefaultConnectionLimit);
-        }
-
-        public async Task UploadAsync(string blobName, IProgress<TransferStatus> progress, Stream contentStream, CancellationToken cancellationToken)
-        {
-            // shard out the incoming stream
-            var chunkSize = DefaultChunkSize;
-            var chunkCount = contentStream.Length / chunkSize;
-            var tasks = new List<Task>();
-            var semaphore = new SemaphoreSlim(MaxParallelism, MaxParallelism);
-            for (var i = 0; i < chunkCount; i++)
-            {
-                tasks.Add(UploadChunkAsync(contentStream, chunkSize, i, semaphore, cancellationToken));
-            }
-
-            await Task.WhenAll(tasks);
-        }
-
-        private async Task UploadChunkAsync(Stream contentStream, int chunkSize, int chunkIndex, SemaphoreSlim semaphore, CancellationToken cancellationToken)
-        {
-            try
-            {
-                semaphore.Wait();
-                var buffer = new byte[chunkSize];
-                await contentStream.ReadAsync(buffer, chunkIndex * chunkSize, chunkSize);
-                var chunkStream = new MemoryStream(buffer);
-                var hashAlgo = SHA256.Create();
-                var hash = hashAlgo.ComputeHash(buffer);
-                var hashString = BitConverter.ToString(hash).Replace("-", string.Empty);
-                await _broker.UploadAsync(hashString, new Progress<TransferStatus>(), chunkStream, cancellationToken);
-            }
-            finally
-            {
-                semaphore.Release();
-            }
-        }
-    }
-
     public class BlobBroker : IBlobBroker
     {
         private Uri _storageUri;
+
         private BlobServiceClient _blobServiceClient;
+
+        private readonly BlobContainerClient containerClient;
 
         public BlobBroker(Uri storageUri)
         {
             _storageUri = storageUri;
             _blobServiceClient = new BlobServiceClient(_storageUri, new DefaultAzureCredential());
+            containerClient = _blobServiceClient.GetBlobContainerClient("objects");
         }
 
-        public async Task<Response> DownloadAsync(string blobName, IProgress<TransferStatus> progress, Stream contentStream, CancellationToken cancellationToken)
+        public async Task<Response> DownloadAsync(string blobName, IProgress<long> progress, Stream contentStream, CancellationToken cancellationToken)
         {
-            var containerClient = _blobServiceClient.GetBlobContainerClient("objects");
-            BlobClient blobClient = containerClient.GetBlobClient(blobName);
-            return await blobClient.DownloadToAsync(contentStream, cancellationToken);
+            var blobClient = containerClient.GetBlockBlobClient(blobName);
+            return await blobClient.DownloadToAsync(contentStream, new BlobDownloadToOptions { ProgressHandler = progress } , cancellationToken);
         }
 
-        public async Task<Response<BlobContentInfo>> UploadAsync(string blobName, IProgress<TransferStatus> progress, Stream contentStream, CancellationToken cancellationToken)
+        public async Task<Response> DownloadAsync(string blobName, IProgress<long> progress, Stream contentStream, long startBytes, long endBytes, CancellationToken cancellationToken)
         {
-            var containerClient = _blobServiceClient.GetBlobContainerClient("objects");
+            var blobClient = containerClient.GetBlockBlobClient(blobName);
+            var result = await blobClient.DownloadStreamingAsync(new HttpRange(startBytes, (endBytes - endBytes)), progressHandler: progress, cancellationToken: cancellationToken);
+            await result.Value.Content.CopyToAsync(contentStream);
+            return result.GetRawResponse();
+        }
+
+        public async Task<Response<BlobContentInfo>> UploadAsync(string blobName, IProgress<long> progress, Stream contentStream, CancellationToken cancellationToken)
+        {
             var blobClient = containerClient.GetBlobClient(blobName);
-            return await blobClient.UploadAsync(contentStream, true, cancellationToken);
+            return await blobClient.UploadAsync(contentStream, new BlobUploadOptions { ProgressHandler = progress }, cancellationToken: cancellationToken);
         }
     }
 }
--- src/CloudLfs.Core/Brokers/IBlobBroker.cs
+++ src/CloudLfs.Core/Brokers/IBlobBroker.cs
@@ -16,7 +16,19 @@ namespace Microsoft.MixedReality.CloudLfs.Brokers
         /// <param name="id">The id of the content.</param>
         /// <param name="contentStream">The stream to write to.</param>
         /// <returns>True if the file was successfully downloaded, false otherwise.</returns>
-        public Task<Response> DownloadAsync(string id, IProgress<TransferStatus> progress, Stream contentStream, CancellationToken cancellationToken);
+        public Task<Response> DownloadAsync(string id, IProgress<long> progress, Stream contentStream, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Download a subsection of the blob.
+        /// </summary>
+        /// <param name="blobName">The blob name.</param>
+        /// <param name="progress">The progress handler, reports how many bytes have been transferred.</param>
+        /// <param name="contentStream">The stream to download to.</param>
+        /// <param name="startBytes">The position in the blob to start at, must be less than blob size.</param>
+        /// <param name="endBytes">The position in the blob to end at, must be less than blob size and greater than <paramref name="endBytes"/></param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The raw response from the blob service.</returns>
+        public Task<Response> DownloadAsync(string blobName, IProgress<long> progress, Stream contentStream, long startBytes, long endBytes, CancellationToken cancellationToken);
 
         /// <summary>
         /// Upload the content to the storage provider. Return FALSE if file could not be uploaded.
@@ -24,6 +36,6 @@ namespace Microsoft.MixedReality.CloudLfs.Brokers
         /// <param name="id">The id of the content.</param>
         /// <param name="contentStream">The stream to read from.</param>
         /// <returns>True if the file was successfully uploaded, false otherwise.</returns>
-        public Task<Response<BlobContentInfo>> UploadAsync(string id, IProgress<TransferStatus> progress, Stream contentStream, CancellationToken cancellationToken);
+        public Task<Response<BlobContentInfo>> UploadAsync(string id, IProgress<long> progress, Stream contentStream, CancellationToken cancellationToken);
     }
 }
