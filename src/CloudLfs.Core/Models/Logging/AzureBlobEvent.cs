cae622c30d9b5937e781eaa83a2ca3dd80c76caf
Daniel Lopez <llopezmejia@microsoft.com>
2022-09-29 17:58:17 -0700

    Telemetry: added AzureBlobEvent class

---
+++ src/CloudLfs.Core/Models/Logging/AzureBlobEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.MixedReality.CloudLfs.Models.Logging
+{
+    public class AzureBlobEvent
+    {
+        public long ElapsedTimeInSeconds { get; set; }
+        public string BlobName { get; set; }
+        public long BlobSize { get; set; }
+        public string OperationType { get; set; }
+    }
+}
